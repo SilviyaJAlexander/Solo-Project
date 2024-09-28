10c0ee3c5116f531737247561563409765bb5152
Silviya <silviyamaruthikkal@gmail.com>
2024-09-28 14:11:32 +0200

    Added computerchoice and  comapared the choices

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -3,7 +3,7 @@
 multiplayers*/
 
 
-console.info("Reading input");
+console.info("Reading input!!!!");
 console.log("Enter your choice \n 1. Rock \n 2. Paper\n 3. Scissors")
 let choice = process.argv[2];
 choice = parseInt(choice);
@@ -24,4 +24,41 @@ switch (choice) {
 }
 console.log(`User choice is: ${choice}`);
 console.log("Now it's computers turn...!");
-let computerChoice = Math.floor(Math.random() * 4);
+let computerChoice = Math.floor(Math.random() * 3) +1;
+switch (computerChoice) {
+    case 1: 
+        console.log('Rock');
+        break;
+    case 2: 
+        console.log('Paper');
+        break;   
+    case 3: 
+        console.log('Scissors');
+        break; 
+    default:
+        console.log('Invalid choice');
+        break;
+     
+}
+console.info("Decision Structure!!!!");
+let winner;
+if (choice === computerChoice){
+    console.log("It's a Tie");
+}   
+else {
+    if( choice ==1 && computerChoice ==2 ||  (computerChoice == 1 && choice ==2)){
+        winner= 'Paper';
+    }
+    else if( choice ==1 && computerChoice ==3 ||  (computerChoice == 1 && choice ==3)){
+        winner= 'Rock';
+    }
+    else if( choice ==2 && computerChoice ==3 ||  (computerChoice == 2 && choice ==3)){
+        winner= 'Scissors';
+    }
+
+}
+//who wins?
+if (winner){
+    
+
+}
